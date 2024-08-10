ffbd3953979d28ee608d393aa9be94d5e4948d46
vienteck <vienteck@protonmail.com>
2024-08-10 12:31:08 -0700

    updated whichkeys mappings

--- after/plugin/whichkeys.lua
+++ after/plugin/whichkeys.lua
@@ -1,21 +1,32 @@
-local wk = require("which-key")
 
-wk.register({
-	["<leader>"] = {
-		f = {
-			name = "+Telescope",
-		},
-		g = {
-			name = "+Github Utilities",
-		},
-		d = {
-			name = "+Debugger",
-		},
-		h = {
-			name = "+GitGutter",
-		},
-    n ={
-      name = "+NerdTree",
-    }
-	},
+-- wk.register({
+-- 	["<leader>"] = {
+-- 		f = {
+-- 			name = "+Telescope",
+-- 		},
+-- 		g = {
+-- 			name = "+Github Utilities",
+-- 		},
+-- 		d = {
+-- 			name = "+Debugger",
+-- 		},
+-- 		h = {
+-- 			name = "+GitGutter",
+-- 		},
+--     n ={
+--       name = "+NerdTree",
+--     }
+-- 	},
+-- })
+
+
+local wk = require("which-key")
+wk.add({
+    { "<leader>d", group = "Debugger" },
+    { "<leader>f", group = "Telescope" },
+    { "<leader>g", group = "Github Utilities" },
+    { "<leader>h", group = "GitGutter" },
+    { "<leader>n", group = "NerdTree" },
+
+
 })
